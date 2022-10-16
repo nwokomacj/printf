9d04eb5d4780d32bc197892af64aaf0d31e0a82b
CHIKELU IKENNA GERALD <myprogramminglive@gmail.com>
2022-10-16 07:45:36 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+*_print - print string as printf
+*@format: number of arguments
+*Return: number of charter
+**/
+
+int _printf(const char *format, ...)
+{
+	va_list p;
+	unsigned int i, result;
+	result = 0;
+	va_start(p, format);
+
+	for (i = 0; format != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			result +=
+		}
+	}
+}
